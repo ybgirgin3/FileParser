ac916ba8d267d8686d3e0a30a315da37106ec450
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-07-08 23:03:24 +0300

    image parsing part is done 🛠

--- utils/image_parser/image.py
+++ utils/image_parser/image.py
@@ -1,2 +1,13 @@
+from PIL import Image
+from pytesseract import pytesseract as pts
+from subprocess import check_output
+
 def image(fp):
-  return f"image file: {fp}"
+  #_pts_ocr = check_output(['which', 'tesseract'])
+  _pts_ocr = "/opt/homebrew/bin/tesseract"
+  pts.tesseract_cmd = _pts_ocr
+
+  img = Image.open(fp)
+  text = pts.image_to_string(fp)
+
+  return text

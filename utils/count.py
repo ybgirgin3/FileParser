b935c3e5e7f5cfa82ab1eb97e1d649cc5ea6dc00
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-07-10 15:34:05 +0300

    beautif'd

--- utils/count.py
+++ utils/count.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 def counter(content):
   "returns count of words in giving content"
   return Counter(content)
--- utils/extra/yaml_data.py
+++ utils/extra/yaml_data.py
@@ -1,8 +1,8 @@
 import yaml
 
-def yaml_parser(fp=None):
-  return yaml.safe_load(open(fp)) #if fp is not None else ""
 
+def yaml_parser(fp=None):
+  return yaml.safe_load(open(fp))  # if fp is not None else ""
 
 # ret = _yaml_parser('utils/configs/tess_paths.yaml')
 # print(ret)

99875fcb1a48b5957549aa9afe612e22b1a2cd23
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-07-10 15:10:51 +0300

    space indentation configured

--- utils/extra/case_derivates.py
+++ utils/extra/case_derivates.py
@@ -1,8 +1,8 @@
 from itertools import product
 
 def derivates(istr):
-    list_ = [(c, c.upper()) if not c.isdigit() else (c,) for c in istr.lower()]
-    return ["".join(item) for item in product(*list_)]
+  list_ = [(c, c.upper()) if not c.isdigit() else (c,) for c in istr.lower()]
+  return ["".join(item) for item in product(*list_)]
 
 
 #import sys
